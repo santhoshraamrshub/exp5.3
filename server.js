3607f69b9055ce49deebbb15a86e4b9bc9c3708a
Santhosh Raam R S <666688santhosh@gmail.com>
2026-03-05 00:10:01 +0530

    Updated UI and features

--- server.js
+++ server.js
@@ -36,7 +36,10 @@ app.get("/category-ratings", async (req,res)=>{
     ]);
     res.json(result);
 });
-app.listen(3000,()=>console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+console.log("Server running on port", PORT);
+});
 app.post("/reset", async (req, res) => {
 await Product.deleteMany({});
 res.json({ message: "All products deleted" });
